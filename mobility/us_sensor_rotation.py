c183e63387e93fea4efad22bf994c0ab7c5c331e
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-11-23 15:30:10 -0500

    sensor rotate complete

--- mobility/us_sensor_rotation.py
+++ mobility/us_sensor_rotation.py
@@ -1,63 +1,72 @@
-# V1.0 ROBOT MOTION AROUND THE GRID TO COVER ALL AREAS
+#Drumming Mechanism Code
 
 import brickpi3
 import time
 from utils import brick
-from utils.brick import BP, Motor, EV3UltrasonicSensor, wait_ready_sensors
+from utils.brick import BP, Motor
 
-# Allocate resources, initial configuration
+#Program parameters
+
+INIT_TIME = 1
+
+#Allocate resources, initial configuration
 
 BP = brickpi3.BrickPi3()
+AUX_MOTOR = BP.PORT_B
 POWER_LIMIT = 80
-SPEED_LIMIT = 200
+SPEED_LIMIT = 720
 DPS = 180
-DRUM_ANGLE = 20
+ROTATION_ANGLE = 200
 US_SENSOR_MOTOR = Motor("A")
-US_SENSOR = EV3UltrasonicSensor(3)
-
-# New parameters for spiral
-ORIENTTODEG = 0.053 / 0.02
 
 
-# Function to initialize motor
-def init_motor(motor: Motor):
+#Function to initialize motor
+def init_motor(motor : Motor):
     try:
+        motor.reset_encoder()
         motor.set_limits(POWER_LIMIT, SPEED_LIMIT)
         motor.set_power(0)
     except IOError as error:
         print(error)
 
+#Function that makes the bot drum
+def sensor_rotate(direction):
+    #Calculate the sleep and limits/position of the motor using the global variables
+    time.sleep(ROTATION_ANGLE/DPS)
+    US_SENSOR_MOTOR.set_limits(dps = DPS)
+    if (direction == "up"):
+        US_SENSOR_MOTOR.set_position_relative(ROTATION_ANGLE)
+    else:
+        US_SENSOR_MOTOR.set_position_relative(-ROTATION_ANGLE)
+    time.sleep(ROTATION_ANGLE/DPS)
 
-# Turns left by the given angle
-def turnSensor(deg):
+
+
+#Entry point -- print instructions
+if __name__=="__main__":
     try:
-        US_SENSOR_MOTOR.set_position_relative(int(deg * ORIENTTODEG))
-    except IOError as error:
-        print(error)
+        print('BrickPi Motor Position Demo:')
 
-
-
-# Entry point -- print instructions
-if __name__ == "__main__":
-    try:
-        wait_ready_sensors(True)
-        print('Basic US Rotation Test')
-
+        
         try:
-            # Initialize the motor and set desired limits
+            #Initialize the motor and set desired limits
             init_motor(US_SENSOR_MOTOR)
-
+            BP.offset_motor_encoder(AUX_MOTOR, BP.get_motor_encoder(AUX_MOTOR))
+            BP.set_motor_limits(AUX_MOTOR, POWER_LIMIT, SPEED_LIMIT)
+            BP.set_motor_power(AUX_MOTOR, 0)
         except IOError as error:
             print(error)
-
+            
+        #Main loop
+        
         try:
-            turnSensor(90)
-            time.sleep(2)
+            sensor_rotate("up")
+            sensor_rotate("down")
 
+        
         except IOError as error:
             print(error)
 
-    # Trigger program exit with ^C
+    #Trigger program exit with ^C
     except KeyboardInterrupt:
         BP.reset_all()
-
