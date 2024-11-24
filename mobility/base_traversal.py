356520d6a576f2541b821d8432a43a2093f922d1
Garrett Woodson <garrettwoodson@icloud.com>
2024-11-24 12:12:40 -0500

    return to trash algorithm implemented

--- mobility/base_traversal.py
+++ mobility/base_traversal.py
@@ -39,11 +39,6 @@ def init_motor(motor : Motor):
 
 #Goes forward until it gets within a certain distance from the wall or trash is detected
 def followWallUntilHit(distFromWall, sideDist):
-    if CSR_sense_trash():
-        print("Trash detected")
-        RIGHT_WHEEL.set_dps(0)
-        LEFT_WHEEL.set_dps(0)
-        return True
     
     print(distFromWall)
     current_dist = US_SENSOR_FRONT.get_value()
@@ -86,7 +81,6 @@ def followWallUntilHit(distFromWall, sideDist):
     RIGHT_WHEEL.set_dps(0)
     LEFT_WHEEL.set_dps(0)
     print("OUTSIDE")
-    return False
     
 #Goes forward dist amount of cm
 def moveDistForward(dist):
@@ -110,121 +104,6 @@ def turnRight(deg):
         LEFT_WHEEL.set_position_relative(int(-deg*ORIENTTODEG))
     except IOError as error:
         print(error)
-
-def goToTrash():
-    
-    # Get Distance To Wall Infront of Robot
-    distanceToForwardWall = US_SENSOR_FRONT.get_value()
-    while distanceToForwardWall == None:
-        distanceToForwardWall = US_SENSOR_FRONT.get_value()
-
-    # Get Distance To Wall to the side of Robot
-    distanceToRightWall = US_SENSOR_RIGHT.get_value()
-    while distanceToRightWall == None:
-        distanceToRightWall = US_SENSOR_RIGHT.get_value()
-
-    distanceToBackWall = 122 - distanceToForwardWall
-    distanceToLeftWall = 122 - distanceToRightWall
-
-    # If the right wall is the closest wall, turn right
-    if distanceToRightWall < distanceToForwardWall and distanceToRightWall < distanceToLeftWall and distanceToRightWall < distanceToBackWall:
-        turnRight(90)
-
-    # If the left wall is the closest wall, turn left
-    elif distanceToLeftWall < distanceToForwardWall and distanceToLeftWall < distanceToRightWall and distanceToLeftWall < distanceToBackWall:
-        turnLeft(90)
-
-     # If the back wall is the closest wall, turn around
-    elif distanceToBackWall < distanceToForwardWall and distanceToBackWall < distanceToRightWall and distanceToBackWall < distanceToLeftWall:
-        turnLeft(180)
-        
-    # If the forward wall is the closest wall, do nothing
-
-    # Move forward until the trash is detected or wall is hit
-    dist = STARTDIST
-    distanceToRightWall = US_SENSOR_RIGHT.get_value()
-    while distanceToRightWall == None:
-        distanceToRightWall = US_SENSOR_RIGHT.get_value()
-    
-    try:
-        #ANGLE DEPENDS ON BATTERY
-        # Travel to closest wall
-        if (followWallUntilHit(dist, distanceToRightWall)):
-            return
-        print("turning left")
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-
-        # Follow wall until trash is detected
-        side = SIDEDIST
-        if (followWallUntilHit(dist, side)):
-            return
-        print("turning left")
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-        if (followWallUntilHit(dist, side)):
-            return
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-        if (followWallUntilHit(dist, side)):
-            return
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-        dist += INCREMENT
-        if (followWallUntilHit(dist, side)):
-            return
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-        side += INCREMENT
-        if (followWallUntilHit(dist, side)):
-            return
-        print("turning left")
-        turnLeft(60)
-        time.sleep(1)
-        moveDistForward(0.3)
-        time.sleep(0.5)
-        turnLeft(20)
-        time.sleep(1)
-
-    except IOError as error:
-        print(error)
-
-
-def CSR_sense_trash():
-    # Based on the sensor, get the median of 20 samples
-    CSR_median_r, CSR_median_g, CSR_median_b = color_sensor_filter(CSR)
-    # Normalize the color sensor data
-    CSR_normalized_r, CSR_normalized_g, CSR_normalized_b = normalize_color_sensor_data(CSR_median_r, CSR_median_g, CSR_median_b)
-    # Get the closest color based on which sensor it is
-    trash_color_center = (0.5379, 0.3761, 0.0861, "TRASH") # Yellow / unnormalized (4.48, 6.30, 14.26)
-    # Calculate the distance to the center
-    distance_to_center = calculate_euclidean_distance((CSR_normalized_r, CSR_normalized_g, CSR_normalized_b), trash_color_center)
-    # if the distance is less than a threshold, then it is water
-    if distance_to_center < 0.1:
-        print("**** CSR: Trash is detected with distance: ", distance_to_center)
-        return True
-    else:
-        return False
 
 #Entry point -- print instructions
 if __name__=="__main__":
